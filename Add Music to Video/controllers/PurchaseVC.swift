866d96fc35b40ec60ea7dc27e096991e4c48ecfd
Tipu <>
2023-08-13 16:54:28 +0600

    video issue solved.

--- Add Music to Video/controllers/PurchaseVC.swift
+++ Add Music to Video/controllers/PurchaseVC.swift
@@ -23,7 +23,7 @@ class PurchaseVC: UIViewController {
         downView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         videoPlay()
     }
-    func videoPlay(){
+    func videoPlay() {
         // Create a video URL (replace with your video URL)
         if let videoURL = Bundle.main.url(forResource: "172807 (1080p)", withExtension: "mp4") {
             // Create an AVPlayer with the video URL
@@ -31,20 +31,41 @@ class PurchaseVC: UIViewController {
             
             // Create an AVPlayerLayer and add it to the videoView's layer
             let playerLayer = AVPlayerLayer(player: player)
-            playerLayer.videoGravity = .resizeAspectFill
-            playerLayer.frame = videoView.bounds
-            videoView.layer.addSublayer(playerLayer)
             
-            // Start playing the video
-            player?.play()
+            let videoAsset = AVAsset(url: videoURL)
+            let videoTrack = videoAsset.tracks(withMediaType: .video).first
             
-            // Loop the video continuously
-            NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player?.currentItem, queue: .main) { [weak self] _ in
-                self?.player?.seek(to: CMTime.zero)
-                self?.player?.play()
+            if let videoTrack = videoTrack {
+                let videoSize = videoTrack.naturalSize
+                let videoTransform = videoTrack.preferredTransform
+                let videoAspectRatio = abs(videoSize.width / videoSize.height)
+                
+                let screenAspectRatio = UIScreen.main.bounds.width / UIScreen.main.bounds.height
+                
+                // Determine videoGravity based on aspect ratio
+                if videoAspectRatio > screenAspectRatio {
+                    playerLayer.videoGravity = .resizeAspectFill
+                } else {
+                    playerLayer.videoGravity = .resizeAspect
+                }
+                
+                playerLayer.frame = videoView.bounds
+                videoView.layer.addSublayer(playerLayer)
+                
+                // Start playing the video
+                player?.play()
+                
+                // Loop the video continuously
+                NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player?.currentItem, queue: .main) { [weak self] _ in
+                    self?.player?.seek(to: CMTime.zero)
+                    self?.player?.play()
+                }
             }
         }
     }
+
+
+
     @IBAction func crossButton(_ sender: UIBarButtonItem) {
         
         dismiss(animated: true)

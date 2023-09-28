7378f41f46ce03727b7d4ede8a4ac79b759c7c3d
Tipu <>
2023-09-28 09:46:19 +0600

    .xib added for crop views

---
+++ Add Music to Video/controllers/editpage/crop/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  Add Music to Video
+//
+//  Created by Tipu on 28/9/23.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Add Music to Video/controllers/editpage/crop/cropView.swift
@@ -0,0 +1,20 @@
+//
+//  cropView.swift
+//  Add Music to Video
+//
+//  Created by Tipu on 27/9/23.
+//
+
+import UIKit
+
+class cropView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

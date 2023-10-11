b184cebb4574fb84bd6852424ce9d1826d2fd0b7
Tipu <>
2023-10-12 02:17:14 +0600

    edit page: thubnailview work

--- Add Music to Video/Controllers/editpage/editPageVc.swift
+++ Add Music to Video/Controllers/editpage/editPageVc.swift
@@ -2,14 +2,14 @@ import UIKit
 import AVFoundation
 import Photos
 
-class editPageVc: UIViewController, CanvasVCDelegate  {
+class editPageVc: UIViewController, CanvasVCDelegate,UIGestureRecognizerDelegate  {
     // Implement the delegate method to handle the selected crop option
     func didSelectCropOption(_ cropOption: CropOption) {
         print("Selected crop option:", cropOption)
         applyCropOptionToVideo(cropOption)
     }
-
-  
+    
+    
     var selectedMusicURL: URL?
     var selectedVideoURL: URL?
     
@@ -24,7 +24,6 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
     
     @IBOutlet weak var editLeftButtoon: UIBarButtonItem!
     
-    var stopButton: UIButton!
     var avPlayer: AVPlayer?
     var isPlaying = false
     
@@ -34,15 +33,19 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
     
     @IBOutlet weak var canvasView: UIView!
     
+    // Add a UILabel to your view controller to display the current time
+    @IBOutlet weak var currentTimeLabel: UILabel!
     
-    //var aiEffectsView: AIEffectsView?
-    //var filtersView: FiltersView?
-    //var fontsView: FontsView?
+    let thumbnailInterval: Double = 4.0 // Display a thumbnail every 5 seconds
+    var thumbnails: [UIImage] = []
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var thumbnelView: UIImageView!
     
     var canvasVC: canvasVC?
     var selectedCropOption: CropOption?
-
-    
+        
     override func viewDidLoad(){
         super.viewDidLoad()
         
@@ -53,14 +56,33 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
         
         SelectedAudioVideoPlay()
         CoontainerViewsSetUp()
-        // Add tap gesture recognizer to videooView
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(videoViewTapped))
-        videooView.addGestureRecognizer(tapGestureRecognizer)
-        stopoButton()
+        
         // Add observer for AVPlayerItemDidPlayToEndTime notification
         NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying(_:)), name: .AVPlayerItemDidPlayToEndTime, object: nil)
+        
         editLeftButtoon.title = "New"
-
+        
+        // Add a tap gesture recognizer to the image views
+        for imageView in scrollView.subviews {
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(thumbnailTapped(_:)))
+            tapGesture.delegate = self
+            imageView.addGestureRecognizer(tapGesture)
+        }
+        
+        // Assuming you have a URL for your video, call setupThumbnailScrollView here
+          if let videoURL = selectedVideoURL {
+              setupThumbnailScrollView(for: videoURL)
+          }
+        
+    }
+    @objc func thumbnailTapped(_ gesture: UITapGestureRecognizer) {
+        if let imageView = gesture.view as? UIImageView, let player = player {
+            let selectedTime = CMTime(seconds: Double(imageView.tag) * thumbnailInterval, preferredTimescale: 1)
+            player.seek(to: selectedTime)
+            
+            // Update the selected thumbnail frame in your thumbnelView
+            thumbnelView.image = thumbnails[imageView.tag]
+        }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "CanvasVC" {
@@ -75,31 +97,22 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
         }
     }
     func CoontainerViewsSetUp(){
-        //aiEffectsView = AIEffectsView()
-        //canvasView = CanvasView()
-        
-        //filtersView = FiltersView()
-        //fontsView = FontsView()
-        
-        // Add custom views as subviews to container views
-        //aiEffectsContainerView.addSubview(aiEffectsView!)
-        //canvasContainerView.addSubview(canvasView!)
-        //filtersContainerView.addSubview(filtersView!)
-        //fontsContainerView.addSubview(fontsView!)
-        
-        // Hide custom views initially
         aiEffectsContainerView.isHidden = true
         canvasView.isHidden = true
         filtersContainerView.isHidden = true
         fontsContainerView.isHidden = true
-        
-        
     }
     
     func SelectedAudioVideoPlay(){
         if let selectedVideoURL = selectedVideoURL {
             videoPlayer = AVPlayer(url: selectedVideoURL)
             let playerLayer = AVPlayerLayer(player: videoPlayer)
+            
+            // Add a time observer to update the current time label
+            videoPlayer?.addPeriodicTimeObserver(forInterval: CMTimeMake(value: 1, timescale: 1), queue: DispatchQueue.main) { [weak self] time in
+                
+                self?.updateCurrentTimeLabel(time)
+            }
             
             playerLayer.videoGravity = .resizeAspect // Set videoGravity to resizeAspect
             videooView.layer.addSublayer(playerLayer)
@@ -121,7 +134,40 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
             }
         }
     }
-    
+    // Function to update the current time label
+    func updateCurrentTimeLabel(_ time: CMTime) {
+        let currentTimeSeconds = CMTimeGetSeconds(time)
+        let hours = Int(currentTimeSeconds / 3600)
+        let minutes = Int((currentTimeSeconds.truncatingRemainder(dividingBy: 3600)) / 60)
+        let seconds = Int(currentTimeSeconds.truncatingRemainder(dividingBy: 60))
+        
+        currentTimeLabel.text = String(format: "%02d:%02d", minutes, seconds)
+    }
+
+    func setupThumbnailScrollView(for videoURL: URL) {
+        thumbnails = generateThumbnails(for: videoURL, at: thumbnailInterval)
+
+        let thumbnailHeight: CGFloat = thumbnelView.frame.size.height
+        let thumbnailAspectRatio: CGFloat = thumbnails[0].size.width / thumbnails[0].size.height
+
+        // Calculate the thumbnail width to maintain the aspect ratio
+        let thumbnailWidth: CGFloat = thumbnailHeight * thumbnailAspectRatio
+
+        var xPosition: CGFloat = 0.0
+
+        for (index, thumbnail) in thumbnails.enumerated() {
+            let imageView = UIImageView(image: thumbnail)
+            imageView.frame = CGRect(x: xPosition, y: 0, width: thumbnailWidth, height: thumbnailHeight)
+            imageView.contentMode = .scaleAspectFit  // Set content mode to maintain aspect ratio
+            imageView.isUserInteractionEnabled = true
+            imageView.tag = index
+            scrollView.addSubview(imageView)
+
+            xPosition += thumbnailWidth
+        }
+
+        scrollView.contentSize = CGSize(width: xPosition, height: thumbnailHeight)
+    }
     
     //MARK: CONTAINERVIEWS SHOW-HIDE
     // Function to show a specific container view with animation
@@ -157,13 +203,8 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
         case 0:
             showContainerView(containerView: aiEffectsContainerView)
         case 1:
-           
+            
             showContainerView(containerView: canvasView)
-            //print("Delegate set to:", canvasVC?.delegate as Any)
-            // Set the delegate after showing the canvasView
-           
-            //performSegue(withIdentifier: "CanvasVC", sender: self)
-         
         case 2:
             if let selectMusicVc = storyboard?.instantiateViewController(withIdentifier: "SelectMusicViewController") as? selectMusicVC {
                 selectMusicVc.selectedVideoURL = selectedVideoURL // Pass the selected video URL
@@ -203,53 +244,6 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
         // Pause the audio player here
         audioPlayer?.pause()
     }
-    func stopoButton(){
-        
-        // Create and configure the stop button
-        stopButton = UIButton(type: .custom)
-        stopButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
-        stopButton.tintColor = UIColor.white
-        stopButton.translatesAutoresizingMaskIntoConstraints = false
-        stopButton.isHidden = true // Initially hide the button
-        
-        // Add the stop button as a subview of videooView
-        videooView.addSubview(stopButton)
-        
-        // Center the stop button in the videooView
-        NSLayoutConstraint.activate([
-            stopButton.centerXAnchor.constraint(equalTo: videooView.centerXAnchor),
-            stopButton.centerYAnchor.constraint(equalTo: videooView.centerYAnchor)
-        ])
-        
-        // Setup AVPlayer for video and audio
-        if let selectedVideoURL = selectedVideoURL {
-            avPlayer = AVPlayer(url: selectedVideoURL)
-            let playerLayer = AVPlayerLayer(player: avPlayer)
-            playerLayer.videoGravity = .resizeAspect
-            videooView.layer.addSublayer(playerLayer)
-            avPlayer?.play()
-            playerLayer.frame = videooView.bounds
-            playerLayer.contentsGravity = .center
-        }
-        
-        stopButton.isUserInteractionEnabled = false
-    }
-    
-    @objc func videoViewTapped() {
-        if let avPlayer = avPlayer {
-            if isPlaying {
-                avPlayer.pause()
-                audioPlayer?.pause()
-                stopButton.isHidden = false
-                isPlaying = false
-            } else {
-                avPlayer.play()
-                audioPlayer?.play()
-                stopButton.isHidden = true
-                isPlaying = true
-            }
-        }
-    }
     
     // Function to pause the audio player
     func pauseAudioPlayer() {
@@ -259,6 +253,30 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
             }
         }
     }
+    //MARK: generateThumbnails
+    func generateThumbnails(for videoURL: URL, at intervals: Double) -> [UIImage] {
+        let asset = AVAsset(url: videoURL)
+        var thumbnails: [UIImage] = []
+
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+        imageGenerator.appliesPreferredTrackTransform = true
+
+        let duration = CMTimeGetSeconds(asset.duration)
+        let interval = CMTime(seconds: intervals, preferredTimescale: 1)
+
+        for time in stride(from: .zero, to: duration, by: intervals) {
+            do {
+                let cgImage = try imageGenerator.copyCGImage(at: CMTime(seconds: time, preferredTimescale: 1), actualTime: nil)
+                let image = UIImage(cgImage: cgImage)
+                thumbnails.append(image)
+            } catch {
+                print("Error generating thumbnail: \(error)")
+            }
+        }
+
+        return thumbnails
+    }
+
     //MARK: NAVBAR BUTTONS
     @IBAction func newButtonTapped(_ sender: UIBarButtonItem) {
         
@@ -284,14 +302,12 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
                 
                 // Call the function to hide the corresponding container view
                 hideEditingOptionForTabBarItem(tabBarItemIndex: selectedIndex)
-               
+                
             }
-           
+            
         }
-       
-        
     }
-    
+    //MARK: SAVE-BUTTON
     // Function to save the video with music and clip the music
     @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
         guard let selectedVideoURL = selectedVideoURL else {
@@ -357,19 +373,18 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
     }
     
     //MARK: applyCropOption
-
     private func applyCropOptionToVideo(_ cropOption: CropOption) {
         selectedCropOption = cropOption
-
+        
         // Calculate the video's aspect ratio
         let videoAspectRatio = cropOption.image.size.width / cropOption.image.size.height
-
+        
         // Calculate the video's width based on the height of the videooView
         let videoHeight = videooView.frame.height
         let videoWidth = videoHeight * videoAspectRatio
-
+        
         print("Video width:", videoWidth, "Video height:", videoHeight)
-
+        
         // Find the AVPlayerLayer
         if let playerLayer = videooView.layer.sublayers?.compactMap({ $0 as? AVPlayerLayer }).first {
             // Center the video within the videooView
@@ -378,9 +393,6 @@ class editPageVc: UIViewController, CanvasVCDelegate  {
             playerLayer.frame = CGRect(x: xOffset, y: yOffset, width: videoWidth, height: videoHeight)
         }
     }
-
-
-
 }
 
 extension editPageVc: UITabBarDelegate {
